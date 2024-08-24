e053c1ad4d9f15fcae1d5b3ac3c03ecfdbd4e816
Fahatmah <fahatmahmabang9@gmail.com>
2024-08-24 19:33:32 +0800

    added display results function
    - display results
    - added results styles
    - event listener for mortgage amount to add commas

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const form = document.getElementById('mortgageForm')
-const mortgageAmount = document.getElementById('mortgage-amount')
+let mortgageAmount = document.getElementById('mortgage-amount')
 const mortgageTerm = document.getElementById('mortgage-term')
 const interestRate = document.getElementById('interest-rate')
 const repaymentButton = document.getElementById('repayment')
 const interestOnlyButton = document.getElementById('interest-only')
 const clearAllButton = document.getElementById('clearAll')
+const mortgageResultsContainer = document.getElementById('mortgageResults')
+const emptyMessageEl = document.querySelector('.empty-message')
 
-clearAllButton.addEventListener('click', clearFormFields)
+mortgageAmount.addEventListener('input', (e) => addCommas(e.target))
+
+clearAllButton.addEventListener('click', () => {
+  clearFormFields()
+  emptyMessageEl.classList.remove('hide')
+  let mortgageResultsEl = document.querySelector('.results-container')
+  if (mortgageResultsContainer.contains(mortgageResultsEl))
+    mortgageResultsContainer.removeChild(mortgageResultsEl)
+})
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   let mortgageAmountVal = mortgageAmount.value
   let mortgageTermVal = mortgageTerm.value
   let interestRateVal = interestRate.value
+  mortgageAmountVal = Number(mortgageAmountVal.split(',').join(''))
 
   validateForm()
 
@@ -43,11 +54,14 @@ function calculateMortgageRepayment(
   numberOfPayments,
   monthlyInterestRate
 ) {
-  let r = monthlyInterestRate / 100 / 12
-  let n = numberOfPayments * 12
-  let m = (principal * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1)
-  const totalRepayment = m * n
-  return [m.toFixed(2), totalRepayment.toFixed(2)].map(Number)
+  if (numberOfPayments > 0 && monthlyInterestRate > 0) {
+    let r = monthlyInterestRate / 100 / 12
+    let n = numberOfPayments * 12
+    let m = (principal * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1)
+    const totalRepayment = +(m * n).toFixed(2)
+    displayResults(+m.toFixed(2), totalRepayment)
+  }
+  // return [m.toFixed(2), totalRepayment.toFixed(2)].map(Number)
   // console.log([m.toFixed(2), totalRepayment.toFixed(2)].map(Number))
 }
 // console.log(
@@ -66,7 +80,11 @@ function calculateMortgageInterestOnly(
   let interestOnlyMonthlyPayment = (mortgageAmount * annualRateDecimal) / 12
   let totalInterestOnlyPayment =
     interestOnlyMonthlyPayment * (interestOnlyYears * 12)
-  return [interestOnlyMonthlyPayment, totalInterestOnlyPayment]
+  displayResults(
+    +interestOnlyMonthlyPayment.toFixed(2),
+    +totalInterestOnlyPayment.toFixed(2)
+  )
+  // return [interestOnlyMonthlyPayment, totalInterestOnlyPayment]
   // console.log([interestOnlyMonthlyPayment, totalInterestOnlyPayment])
 }
 // console.log(calculateMortgageInterestOnly(400000, 3.75, 5))
@@ -138,6 +156,61 @@ function createErrorMessageEl(el) {
 
   return el
 }
+
+function displayResults(monthlyRepayment, totalRepayment) {
+  const resultsContainer = document.querySelector('.results-container')
+  let resultsContainerEl = document.createElement('div')
+  resultsContainerEl.classList.add('results-container', 'fl-c')
+  resultsContainerEl.innerHTML = `
+                  <div class="body-text">
+                    <h3>Your results</h3>
+                    <p>
+                      Your results are show below based on the information you
+                      provided. To adjust the results, edit the form and click
+                      "calculate repayments" again.
+                    </p>
+                  </div>
+
+                  <div class="results fl-c">
+                    <!-- monthly repayments -->
+                    <div class="monthly-repayments">
+                      <p>Your monthly repayments</p>
+                      <h4 id="monthlyRepayment">£${addCommas(
+                        monthlyRepayment
+                      )}</h4>
+                    </div>
+                    <hr class="line" />
+                    <!-- total repay over the term -->
+                    <div class="total-repayments">
+                      <p>Total you'll repay over the term</p>
+                      <h5 id="totalRepayment">£${addCommas(totalRepayment)}</h5>
+                    </div>
+                  </div>
+                `
+  if (resultsContainer) {
+    mortgageResultsContainer.removeChild(resultsContainer)
+    mortgageResultsContainer.append(resultsContainerEl)
+  } else {
+    mortgageResultsContainer.append(resultsContainerEl)
+  }
+  emptyMessageEl.classList.add('hide')
+}
+
+function addCommas(el) {
+  let regex = /\B(?=(\d{3})+(?!\d))/g
+  if (typeof el === 'number') return el.toString().replace(regex, ',')
+
+  // check if el is an HTML element (e.g., e.target)
+  if (el instanceof HTMLElement) {
+    let val = el.value.replace(/[^0-9]/g, '')
+
+    const parts = el.value.replace(/[^0-9]/g, '').split('.')
+    parts[0] = parts[0].replace(regex, ',')
+
+    el.value = parts.join('.')
+  }
+}
+
 // TODO:
-// DISPLAY RESULTS
+// VALIDATE INPUTS e.g, 0 in mortgage term or interest rate if so, display 'invalid input'
 // DESIGN RESPONSIVENESS
